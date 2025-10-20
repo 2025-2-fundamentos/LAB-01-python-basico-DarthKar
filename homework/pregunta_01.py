7d4f14c8ef1167b5ef48125ea5459bffb98ae70d
Miguel Angel Peña Marin <mipenama@unal.edu.co>
2025-10-19 21:47:42 -0500

    a

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,17 +5,19 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import os
 
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
     Rta/
     214
-
     """
-    suma = 0
-    with open('files\input\data.csv') as archivo:
-        for linea in archivo.readlines():
-            suma+=int(linea.split()[1])
-    return suma
+    dir_actual = os.path.dirname(__file__)
+    ruta = os.path.join(dir_actual, 'files', 'input', 'data.csv')
 
+    suma = 0
+    with open(ruta) as archivo:
+        for linea in archivo:
+            suma += int(linea.split()[1])
+    return suma
---
+++ homework/ruta.py
@@ -0,0 +1,9 @@
+import os
+
+
+dir_actual = os.path.dirname(__file__)
+
+ruta = os.path.join(dir_actual, 'files', 'input', 'data.csv')
+
+
+print(ruta)
