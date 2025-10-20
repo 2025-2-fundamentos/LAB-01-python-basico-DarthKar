4f046d2c0e1789f28f56f720802a8ec2edfa75f7
Miguel Angel Peña Marin <mipenama@unal.edu.co>
2025-10-19 21:53:20 -0500

    Update pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,21 +1,18 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
-
+import os
 
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
     Rta/
     214
-
     """
-    suma = 0
-    with open(r'files\input\data.csv') as archivo:
-        for linea in archivo.readlines():
-            suma+=int(linea.split()[1])
-    return suma
 
+    dir_actual = os.path.dirname(__file__)
+
+    ruta = os.path.join(dir_actual, 'files', 'input', 'data.csv')
+
+    suma = 0
+    with open(ruta) as archivo:
+        for linea in archivo:
+            suma += int(linea.split()[1])
+    return suma
